f0c33b42af84154e6e993c4d166dbe3b5363ca5f
prabhas-code <prabhasmadipadiga@gmail.com>
2026-01-04 22:55:33 +0530

    updated backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://usermanagement-backend-21jh.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
