cbd9e2b38410819b0f001e182a46f4e4f37c2cda
prabhas-code <prabhasmadipadiga@gmail.com>
2026-01-05 08:42:47 +0530

    fixed axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://prabhas-user-management-frontend.vercel.app",
 });
 
 api.interceptors.request.use((config) => {
